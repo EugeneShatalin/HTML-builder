e5a4e3fb2f04f7785bf59ce663b32ebcddcc0f58
EugeneShatalin <shatalin_ei@mail.ru>
2021-11-08 22:03:54 +0600

    fead: add solutions for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fsProm = require('fs/promises');
+const path = require('path');
+const files = path.join(__dirname, 'files');
+const filesCopy = path.join(__dirname, 'files-copy');
 
+async function mkCopyDir() {
+  await fsProm.rmdir(filesCopy, {recursive: true});
+  await fsProm.mkdir(filesCopy, {recursive: true});
+  const filesObj = await fsProm.readdir(files, {withFileTypes: true});
+  for (let i = 0; i < filesObj.length; i++) {
+    if (filesObj[i].isFile()) {
+      const from = path.join(files, filesObj[i].name);
+      const to = path.join(filesCopy, filesObj[i].name);
+      await fsProm.copyFile(from, to);
+    }
+  }
+}
+
+mkCopyDir();
