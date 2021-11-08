4b41afe807d7e7d87b52f6da4cf1803aa2e8b09f
EugeneShatalin <shatalin_ei@mail.ru>
2021-11-08 22:07:25 +0600

    fead: add solutions for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,18 @@
-const fsProm = require('fs/promises');
-const path = require('path');
-const files = path.join(__dirname, 'files');
-const filesCopy = path.join(__dirname, 'files-copy');
+const fsProm = require("fs/promises");
+const path = require("path");
+const files = path.join(__dirname, "files");
+const filesCopy = path.join(__dirname, "files-copy");
 
 async function mkCopyDir() {
-  await fsProm.rmdir(filesCopy, {recursive: true});
-  await fsProm.mkdir(filesCopy, {recursive: true});
-  const filesObj = await fsProm.readdir(files, {withFileTypes: true});
+  await fsProm.rmdir(filesCopy, { recursive: true });
+  await fsProm.mkdir(filesCopy, { recursive: true });
+  const filesObj = await fsProm.readdir(files, { withFileTypes: true });
   for (let i = 0; i < filesObj.length; i++) {
     if (filesObj[i].isFile()) {
-      const from = path.join(files, filesObj[i].name);
-      const to = path.join(filesCopy, filesObj[i].name);
-      await fsProm.copyFile(from, to);
+      await fsProm.copyFile(
+        path.join(files, filesObj[i].name),
+        path.join(filesCopy, filesObj[i].name)
+      );
     }
   }
 }
