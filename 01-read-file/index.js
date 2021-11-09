a5fcfa0fd6e181dafe4867510238853936ed61c4
EugeneShatalin <shatalin_ei@mail.ru>
2021-11-09 23:52:15 +0600

    fix: fixed solutions for 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var stream = new fs.ReadStream("./01-read-file/text.txt", {encoding: 'utf-8'});
+var stream = new fs.ReadStream("01-read-file/text.txt", {encoding: 'utf-8'});
 
 stream.on('readable', function(){
     var data = stream.read();
