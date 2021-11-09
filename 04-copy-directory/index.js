e4d83fb749310a8f70373427607ab71989fc769b
EugeneShatalin <shatalin_ei@mail.ru>
2021-11-09 23:55:50 +0600

    fix: fixed solutions for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,7 +4,7 @@ const files = path.join(__dirname, "files");
 const filesCopy = path.join(__dirname, "files-copy");
 
 async function mkCopyDir() {
-  await fsProm.rmdir(filesCopy, { recursive: true });
+ /*  await fsProm.rmdir(filesCopy, { recursive: true }); */
   await fsProm.mkdir(filesCopy, { recursive: true });
   const filesObj = await fsProm.readdir(files, { withFileTypes: true });
   for (let i = 0; i < filesObj.length; i++) {
