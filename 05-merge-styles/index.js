23068ccd85c3d980031d8a63cc66a83fa6f02730
EugeneShatalin <shatalin_ei@mail.ru>
2021-11-10 22:56:31 +0600

    fix: fixed solutions for 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,21 +1,25 @@
-const fs = require('fs');
-const path = require('path');
-const stylesFolder = path.join(__dirname, 'styles');
-const projectDist = path.join(__dirname, 'project-dist');
-const bundleCSS = path.join(projectDist, 'bundle.css');
+const fs = require("fs");
+const path = require("path");
+const stylesFolder = path.join(__dirname, "styles");
+const projectDist = path.join(__dirname, "project-dist");
+const bundleCSS = path.join(projectDist, "bundle.css");
 
-const filePath = path.join(__dirname, 'text.txt');
+const filePath = path.join(__dirname, "project-dist/bundle.css");
 
-fs.writeFile(filePath, "", function(err){
+fs.writeFile(filePath, "", function (err) {
   if (err) {
-      console.log(err);
+    console.log(err);
   }
 });
 
-fs.readdir(stylesFolder, {withFileTypes: true}, (error, files) => {
+fs.rm(bundleCSS, (error) => {
+  if (error) throw error;
+});
+
+fs.readdir(stylesFolder, { withFileTypes: true }, (error, files) => {
   if (error) throw error;
   for (const file of files) {
-    if (file.isFile() && path.extname(file.name).slice(1) === 'css') {
+    if (file.isFile() && path.extname(file.name).slice(1) === "css") {
       fs.readFile(path.join(stylesFolder, file.name), (error, data) => {
         if (error) throw error;
         fs.appendFile(bundleCSS, data, (error) => {
